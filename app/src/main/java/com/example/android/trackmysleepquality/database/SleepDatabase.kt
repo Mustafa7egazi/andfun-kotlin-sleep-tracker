37726283dc8112831b4fd7e3e51fc63c4ff04ab6
Mustafa R. Hegazy <mustafareda3210@gmail.com>
2023-02-12 18:51:04 +0200

    Added DAO and Database classes

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabase.kt
@@ -15,3 +15,33 @@
  */
 
 package com.example.android.trackmysleepquality.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+@Database(entities = [SleepNight::class], version = 1, exportSchema = false)
+abstract class SleepDatabase:RoomDatabase(){
+    abstract val sleepDatabaseDao:SleepDatabaseDao
+
+    companion object{
+        @Volatile // makes sure that instance value is up tp date to all threads
+        private var INSTANCE:SleepDatabase? = null
+        fun getInstance(context:Context):SleepDatabase{
+            synchronized(this){ //makes sure that only one thread can access or have an instance of DB
+                var instance = INSTANCE
+                if (instance == null){
+                    instance = Room.databaseBuilder(
+                            context.applicationContext,
+                            SleepDatabase::class.java,
+                            "sleep_history_database"
+                    )
+                            .fallbackToDestructiveMigration()
+                            .build()
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
+}
